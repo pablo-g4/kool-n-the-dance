a615729c72ae4fcd1a61f956dd43500347b12d90
Ben <benbugarindev@gmail.com>
2023-01-25 14:00:51 +0100

    add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import Home from './Pages/Home';
 import Topics from './Pages/Topics';
 import Layout from './Pages/Layouts/Layout';
 import './App.css';
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import {
   Route,
