f1117f00990c76ea8f522c0b939e8dab9df2b2e2
Ju.mlts <JKM@live.fr>
2023-01-26 16:41:22 +0100

    Header

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { SocialIcon } from 'react-social-icons';
 
 const Navbar = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light">
                 <a className="navbar-brand" href="#">KOOL 'N' THE DANCE</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -12,25 +12,26 @@ const Navbar = () => {
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Planning <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#">Something else here</a>
-                            </div>
+                            <a className="nav-link" href="#">Cours</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" href="#">Disabled</a>
+                            <a className="nav-link" href="#">Galerie</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Actualité</a>
+                        </li>
+                        <li>
+                            <a> <SocialIcon network="tiktok" bgColor="#FFFFFF" style={{marginLeft: "150px", marginRight: "25px", height: 30, width: 30, marginTop: "2%"}} /> </a>
+                        </li>
+                        <li>
+                            <a> <SocialIcon network="twitter" bgColor="#FFFFFF" style={{marginRight: "25px", height: 30, width: 30, marginTop: "3%"}} /> </a>
+                        </li>
+                        <li>
+                            <a> <SocialIcon network="facebook" bgColor="#FFFFFF" style={{marginRight: "25px", height: 30, width: 30, marginTop: "3%"}} /> </a>
                         </li>
                     </ul>
                 </div>
