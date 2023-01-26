f48e1271d32fc1476a164c684c6792b1d71c98c7
danyGoncalvesNovais <91186956+danyGoncalvesNovais@users.noreply.github.com>
2023-01-26 17:00:47 +0100

    debug

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,49 +3,6 @@ import { SocialIcon } from "react-social-icons";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 const position = [51.505, -0.09];
 const Footer = () => {
-<<<<<<< HEAD
-  return (
-    <div className="footer col-xl-12 col-xs-12">
-      <div className="column col-xl-3">
-        <h1 className="footerH1">Plan du site</h1>
-        <span className="text">
-          <a>Accueil</a>
-        </span>
-        <span className="text">
-          <a>Planning</a>
-        </span>
-        <span className="text">
-          <a>Cours</a>
-        </span>
-        <span className="text">
-          <a>Galerie</a>
-        </span>
-        <span className="text">
-          <a>Actualté</a>
-        </span>
-      </div>
-      <div className="column col-xl-3 col-xs-12">
-        <h1 className="footerH1">Contact</h1>
-        <span className="text">Accueil</span>
-        <span className="text">koolnthedance@gmail.com</span>
-        <span className="text">9H-21</span>
-        <div className="socialIcon">
-          <SocialIcon
-            network="tiktok"
-            bgColor="#FFFFFF"
-            style={{ marginRight: "5px" }}
-          />
-          <SocialIcon
-            network="twitter"
-            bgColor="#FFFFFF"
-            style={{ marginRight: "5px" }}
-          />
-          <SocialIcon
-            network="facebook"
-            bgColor="#FFFFFF"
-            style={{ marginRight: "5px" }}
-          />
-=======
     return (
         <div className='footer d-flex'>
             <div className='col-lg-3 col-md-12'>
@@ -93,25 +50,9 @@ const Footer = () => {
                 </div>
             </div>
             
->>>>>>> 6108e628d658b00464071da112654a123f9a8ee9
         </div>
-      </div>
-      <div className="map col-xl col-xs-12">
-        <div className="map">
-          <MapContainer
-            center={[51.505, -0.09]}
-            zoom={15}
-            scrollWheelZoom={false}
-            className="leaflet-container2"
-          >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-          </MapContainer>
-        </div>
-      </div>
-    </div>
+
+  
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import "../src/Components/Footer/Footer.css";
-import "../src/Pages/Actualite/Actualite.css";
-import "./Components/Footer/Footer.css";
-import App from "./App";
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
@@ -17,7 +7,6 @@ import './index.css';
 import './Components/Footer/Footer.css'
 import './Components/Navbar/Navbar.css'
 import App from './App';
->>>>>>> 6108e628d658b00464071da112654a123f9a8ee9
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
