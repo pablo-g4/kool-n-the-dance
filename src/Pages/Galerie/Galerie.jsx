f18bb7a51cba1c3873e55b4f4e83be4af1a9c3c2
thron91 <97443017+thron91@users.noreply.github.com>
2023-01-26 16:07:48 +0100

    Update Galerie.jsx

--- src/Pages/Galerie/Galerie.jsx
+++ src/Pages/Galerie/Galerie.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import Image1 from '../../images/Galerie/DAN_0568inv.png'
+import Image2 from '../../images/Galerie/DAN_0616inv-1.png'
+import Image3 from '../../images/Galerie/DAN_0809inv.png'
+import Image4 from '../../images/Galerie/DAN_1169inv.png'
+import Image5 from '../../images/Galerie/DAN_1465inv.png'
+
 
 const Galerie = () => {
   return (
@@ -11,13 +17,19 @@ const Galerie = () => {
           <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                <img class="d-block w-100" src="..." alt="First slide"/>
+                <img class="d-block w-100" src={Image1} alt="premiere slide"/>
                 </div>
                 <div class="carousel-item">
-                <img class="d-block w-100" src="..." alt="Second slide"/>
+                <img class="d-block w-100" src={Image2} alt="deuxieme slide"/>
                 </div>
                 <div class="carousel-item">
-                <img class="d-block w-100" src="..." alt="Third slide"/>
+                <img class="d-block w-100" src={Image3} alt="troisieme slide"/>
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src={Image4} alt="quatrieme slide"/>
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src={Image5} alt="cinquieme slide"/>
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
