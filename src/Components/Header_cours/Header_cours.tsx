3dba1ec0804c009a9334a011f555c82b1484077d
danyGoncalvesNovais <91186956+danyGoncalvesNovais@users.noreply.github.com>
2023-03-07 13:43:40 +0100

    cards

--- src/Components/Header_cours/Header_cours.tsx
+++ src/Components/Header_cours/Header_cours.tsx
@@ -3,24 +3,29 @@ import "./Header_cours.css";
 import image from "./test.png";
 
 const Header_cours = () => {
-  const [isShown, setIsShown] = useState(false);
+const [isShown, setIsShown] = useState(false);
+const [isHovering, setIsHovering] = useState(false);
+const handleMouseEnter = () => {setIsHovering(true);};
+const handleMouseLeave = () => {setIsHovering(false);};
+
+
+
+  
 
     return (
         <>
           <div className="groupe">
-            <div className="card"
-              onMouseEnter={() => console.log("test")}
-            >
+            <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{transform: isHovering ? 'translate(50px, 0px)' : 'none',}}>
               <img src={image} className="card-img-top" alt="..." />
             </div>
-            <div className="textdanses">
+            <div className="textdanses" style={{transform: isHovering ? 'translate(50px, 0px)' : 'none',}}>
               <p>Danses</p>
             </div>
-            <div className="card">
+            <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{transform: isHovering ? 'translate(50px, 0px)' : 'none',}}>
               <img src={image} className="card-img-top" /> 
             </div>
             <div className="card">
-              <img src={image} className="card-img-top" alt="..." />
+              <img src={image} className="card-img-top" alt="..." onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  style={{transform: isHovering ? 'translate(50px, 50px)' : 'none',}}/>
             </div>
           </div>
         </>
