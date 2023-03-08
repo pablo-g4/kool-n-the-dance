eba14e9cbcb87c68ce7b86255a001e59cf57968c
Nathan Hyppolite <ihyppolite971@gmail.com>
2023-03-08 16:51:43 +0100

    ajoute du menu admin

--- src/Components/AdminSidebar/AdminSidebar.tsx
+++ src/Components/AdminSidebar/AdminSidebar.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React from "react";
+import "./AdminSidebar.css";
+
 const AdminSidebar = () => {
-    return (
-        <div className="aSidebar">
-        <div className="aSidebar_logo">img</div>
-        <a className="aSidebar_onglet currentPage" href="#">
-            <p className="aSidebar_text">Gestion Général</p>
-        </a>
-        <a className="aSidebar_onglet" href="#"><p className="aSidebar_text">Gestion Cours</p></a>
-        <a className="aSidebar_onglet" href="#"><p className="aSidebar_text">Gestion Planning</p></a>
-        <a className="aSidebar_onglet" href="#"><p className="aSidebar_text">Gestion Galerie</p></a>
-        <a className="aSidebar_onglet" href="#"><p className="aSidebar_text">Gestion Actualité</p></a>
-        <a className="aSidebar_onglet" href="#"><p className="aSidebar_text">Gestion Forfait</p></a>
+  return (
+    <div className="aSidebar">
+      <div className="aSidebar_logo">img</div>
+      <a className="aSidebar_onglet currentPage" href="#">
+        <p className="aSidebar_text">Gestion Général</p>
+      </a>
+      <a className="aSidebar_onglet" href="#">
+        <p className="aSidebar_text">Gestion Cours</p>
+      </a>
+      <a className="aSidebar_onglet" href="#">
+        <p className="aSidebar_text">Gestion Planning</p>
+      </a>
+      <a className="aSidebar_onglet" href="#">
+        <p className="aSidebar_text">Gestion Galerie</p>
+      </a>
+      <a className="aSidebar_onglet" href="#">
+        <p className="aSidebar_text">Gestion Actualité</p>
+      </a>
+      <a className="aSidebar_onglet" href="#">
+        <p className="aSidebar_text">Gestion Forfait</p>
+      </a>
     </div>
-    )
-}
+  );
+};
 export default AdminSidebar;
--- src/Pages/Admin/General/General.tsx
+++ src/Pages/Admin/General/General.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import DropZone from "../../../Components/DropZone/DropZone";
 import "./general.css";
 import { useForm, Resolver } from "react-hook-form";
+import AdminSidebar from "../../../Components/AdminSidebar/AdminSidebar";
 
 const General = () => {
   const {
@@ -18,79 +19,82 @@ const General = () => {
   };
   return (
     <>
-      <div className="m-5   ">
+      <div className="d-flex flex-column- ">
         <div>
-          <DropZone setFiles={setFile} />
-          <button className="w-44 bg-grey-900 p-2 my-5 rounded-md">
-            mettre vos fichiers
-          </button>
+          <AdminSidebar></AdminSidebar>
         </div>
-
-        <h2 className=" text-aligne ">Contact</h2>
-        <div className="d-flex justify-content-between mt-4">
+        <div className=" m-5 box-body ">
           <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="d-flex  ">
-                <label>Telphone : </label>
-                <p> +33 1234567890</p>
-              </div>
-              <input className="input-size" name="tel" type="tel" />
-              {errors.email && <span>Ce champs est requis</span>}
-              <button className="btn btn-dark mx-2" type="submit">
-                Modifier
-              </button>
-            </form>
+            <DropZone setFiles={setFile} />
           </div>
-          <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="d-flex  ">
-                <label> Horaire :</label>
-                <p> 9h-21h</p>
+          <div className="separateur">
+            <h2 className=" text-deco-h2   ">Contact</h2>
+            <div className="d-flex justify-content-between mt-4">
+              <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="d-flex  ">
+                    <label className="bold">Telphone : </label>
+                    <p className="light"> +33 1234567890</p>
+                  </div>
+                  <input className="input-size" name="tel" type="tel" />
+                  {errors.email && <span>Ce champs est requis</span>}
+                  <button className="btn btn-dark mx-2" type="submit">
+                    Modifier
+                  </button>
+                </form>
               </div>
-              <input className="input-size" name="horaire" type="text" />
-              <button className="btn btn-dark mx-2" type="submit">
-                Modifier
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="d-flex justify-content-between mt-4">
-          <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="d-flex  ">
-                <label>TickTock : </label>
-                <p> lien ticktock</p>
+              <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="d-flex  ">
+                    <label className="bold"> Horaire :</label>
+                    <p className="light"> 9h-21h</p>
+                  </div>
+                  <input className="input-size" name="horaire" type="text" />
+                  <button className="btn btn-dark mx-2" type="submit">
+                    Modifier
+                  </button>
+                </form>
               </div>
-              <input className="input-size" name="tiktok" type="text" />
-              <button className="btn btn-dark  mx-2 " type="submit">
-                Modifier
-              </button>
-            </form>
-          </div>
-          <div className="d-flex justify-content-around">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="d-flex  ">
-                <label>Facebook : </label>
-                <p> lien Facebook</p>
-              </div>
-              <input className="input-size" name="facebook" type="text" />
-              <button className="btn btn-dark mx-1" type="submit">
-                Modifier
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="d-flex  flex-row-reverse  mt-4 me-4">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="d-flex  ">
-              <label>Twitter : </label>
-              <p> lien twitter</p>
             </div>
-            <input className="input-size" name="twitter" type="text" />
-            <button className="btn btn-dark mx-1" type="submit">
-              Modifier
-            </button>
-          </form>
+            <div className="d-flex justify-content-between mt-4">
+              <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="d-flex  ">
+                    <label className="bold">TickTock : </label>
+                    <p className="light"> lien ticktock</p>
+                  </div>
+                  <input className="input-size" name="tiktok" type="text" />
+                  <button className="btn btn-dark  mx-2 " type="submit">
+                    Modifier
+                  </button>
+                </form>
+              </div>
+              <div className="d-flex justify-content-around">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="d-flex  ">
+                    <label className="bold">Facebook : </label>
+                    <p className="light"> lien Facebook</p>
+                  </div>
+                  <input className="input-size" name="facebook" type="text" />
+                  <button className="btn btn-dark mx-1" type="submit">
+                    Modifier
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="d-flex  flex-row-reverse  mt-4 me-4">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="d-flex  ">
+                  <label className="bold">Twitter : </label>
+                  <p className="light"> lien twitter</p>
+                </div>
+                <input className="input-size" name="twitter" type="text" />
+                <button className="btn btn-dark mx-1" type="submit">
+                  Modifier
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </>
