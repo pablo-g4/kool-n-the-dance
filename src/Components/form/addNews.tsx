0ec8495721e3216aa87c971e455f1bc99831ea46
thron91 <97443017+thron91@users.noreply.github.com>
2023-03-08 18:18:37 +0100

    css add news

--- src/Components/form/addNews.tsx
+++ src/Components/form/addNews.tsx
@@ -1,27 +1,43 @@
-import { Button } from '@mantine/core'
 import React from 'react'
+import './news.css'
+import attachmentIcon from './attach-16.png'
 
 const addNews = () => {
   return (
-    <form action='' method=''>
-        <label>Titre</label>
-        <input type="text" name='titre' required></input><br></br>
+    <div className="card">
+      <div className='article'>
+        <a className='txtTitre'>Ajouter Article</a>
 
-        <label>Description</label>
-        <input type="text" name='description' required></input><br></br>
+        <form action='' method=''>
+            <label className='txtLabel' htmlFor='titreArticle'>Titre Article : </label><br></br><br></br>
+            <input id="titreArticle" type="text" name='titre' placeholder="Titre Article" required></input><br></br><br></br>
 
-        <label>Ajouter un fichier</label>
-        <input type="file" name='ajoutFichier'></input><br></br>
 
-        <label>active ?</label><br></br>
+            <button className='btnAjout'><img src={attachmentIcon}></img> Ajouter une pièce jointe</button>
+            <input type="file" id='file' className='file' name='ajoutFichier' accept="image/png, image/jpg, image/gif, image/jpeg"></input><br></br><br></br>
+            <br></br><br></br>
+            <label className='txtLabel labelDesc' htmlFor='desc'>Description : </label>
+            <textarea rows={6} cols={79} name='description' className='desc' required></textarea><br></br><br></br>
+            
 
-        <input type="radio" id="active" name="active" value="true" />
-        <label>Oui</label><br></br>
-        <input type="radio" id="avtive" name="active" value="false" />
-        <label>Non</label><br></br>
 
-        <input type="submit"></input><br></br>
-    </form>
+            <br></br>
+            <a href=''><input type="button" value='Annuler' className='btnNoir'></input></a>
+            <input type="submit" value='Valider' className='btnRouge'></input>
+            <br></br><br></br>
+            <div className='visible'>
+              <label>Visible ?</label>
+
+              <input type="radio" id="active" name="active" value="true" />
+              <label>Oui</label>
+              <input type="radio" id="avtive" name="active" value="false" />
+              <label>Non</label>
+            </div>
+
+        </form>
+      </div>
+
+    </div>
   )
 }
 
