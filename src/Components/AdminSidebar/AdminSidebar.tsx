c0ab5d7b1dbd2548e18672b25859285504dba11c
Allan COSTES <allancts.compte@gmail.com>
2023-03-08 16:23:55 +0100

    add Admin Sidebar

---
+++ src/Components/AdminSidebar/AdminSidebar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const AdminSidebar = () => {
+    return (
+        <div className="aSidebar">
+        <div className="aSidebar_logo">img</div>
+        <a className="aSidebar_onglet currentPage" href="#">
+            <p className="aSidebar_text">Gestion Général</p>
+        </a>
+        <a className="aSidebar_onglet" href="#"><p className="aSidebar_text">Gestion Cours</p></a>
+        <a className="aSidebar_onglet" href="#"><p className="aSidebar_text">Gestion Planning</p></a>
+        <a className="aSidebar_onglet" href="#"><p className="aSidebar_text">Gestion Galerie</p></a>
+        <a className="aSidebar_onglet" href="#"><p className="aSidebar_text">Gestion Actualité</p></a>
+        <a className="aSidebar_onglet" href="#"><p className="aSidebar_text">Gestion Forfait</p></a>
+    </div>
+    )
+}
+export default AdminSidebar;
