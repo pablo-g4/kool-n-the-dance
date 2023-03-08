9760be6763159ae05f7025885c9b71db67ca5523
thron91 <97443017+thron91@users.noreply.github.com>
2023-03-08 08:46:04 +0100

    test visualiseuse

--- src/Pages/Galerie/Galerie.jsx
+++ src/Pages/Galerie/Galerie.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react';
 import Footer from '../../Components/Footer/Footer'
 import { Carousel } from '@mantine/carousel';
 import carousel1  from '../../Assets/Images/carousel/carousel1.png';
@@ -7,16 +7,67 @@ import carousel3 from '../../Assets/Images/carousel/carousel3.png';
 import carousel4  from '../../Assets/Images/carousel/carousel4.png';
 import carousel5  from '../../Assets/Images/carousel/carousel5.png';
 import carousel6  from '../../Assets/Images/carousel/carousel6.png';
+import { render } from 'react-dom';
+import ImageViewer from 'react-simple-image-viewer';
+
+
 
 
 const Galerie = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+  const images = [
+    carousel1,
+    carousel2,
+    carousel3,
+    carousel4,
+    carousel5,
+    carousel6,
+  ];
+
+  const openImageViewer = useCallback((index) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
+
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
   return (
+    
     <div className='body'>
       <div className="titre">Galerie</div>
+
+        <div className='visualiseuse'>
+
+          {images.map((src, index) => (
+          <img
+            src={ src }
+            onClick={ () => openImageViewer(index) }
+            width="100"
+            key={ index }
+            style={{ margin: '2px' }}
+            alt=""
+          />
+        ))}
+
+        {isViewerOpen && (
+          <ImageViewer
+            src={ images }
+            currentIndex={ currentImage }
+            disableScroll={ false }
+            closeOnClickOutside={ true }
+            onClose={ closeImageViewer }
+          />
+        )}
+
+        </div>
+
         <div className='carou'>
-          <Carousel orientation="vertical" loop="true" height={545} maw={320} mx="left"  slideSize="13.5%" initialSlide={2} >
+          <Carousel orientation="vertical" loop="true" height={545} maw={320} dragFree slideSize="13.5%" initialSlide={2} >
             <Carousel.Slide>
-              <img src={carousel1} height="110.2px" width="180px"></img>
+              <img src={carousel1} height="110.2px" width="180px" ></img>
             </Carousel.Slide>
             <Carousel.Slide>
               <img src={carousel2} height="110.2px" width="180px"></img>
@@ -31,7 +82,7 @@ const Galerie = () => {
               <img src={carousel5} height="110.2px" width="180px"></img>
             </Carousel.Slide>
             <Carousel.Slide>
-              <img src={carousel6} height="110.2px" width="180px"></img>
+              <img src={carousel6} height="110.2px" width="180px" ></img>
             </Carousel.Slide>
           </Carousel>
         </div>
@@ -40,3 +91,4 @@ const Galerie = () => {
 }
 
 export default Galerie
+
