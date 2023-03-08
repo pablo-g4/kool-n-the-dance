f1e675c053f11fca5c8c128569654c94072b468a
Ben <benbugarindev@gmail.com>
2023-03-08 19:33:09 +0100

    fix carrousel css

--- src/Components/Carrousel_Activite/Carrousel.tsx
+++ src/Components/Carrousel_Activite/Carrousel.tsx
@@ -8,7 +8,7 @@ import SwiperCore, { Navigation } from 'swiper';
 import 'swiper/swiper.min.css';
 import 'swiper/swiper-bundle.css';
 import 'swiper/swiper.min.css';
-import image from './Groupe 46.png';
+import logo from './Groupe 46.png';
 
 
 
@@ -17,63 +17,63 @@ import image from './Groupe 46.png';
 import 'swiper/css';
 
 const slides = [
-  <SwiperSlide key={3}><img src={img}/></SwiperSlide>,
-  <SwiperSlide key={5}><img src={img}/></SwiperSlide>,
-  <SwiperSlide key={4}><img src={img}/></SwiperSlide>,
-  <SwiperSlide key={2}><img src={img}/></SwiperSlide>,
-  <SwiperSlide key={1}><img src={img}/></SwiperSlide>,
+  <SwiperSlide key={3}><img src={img} /></SwiperSlide>,
+  <SwiperSlide key={5}><img src={img} /></SwiperSlide>,
+  <SwiperSlide key={4}><img src={img} /></SwiperSlide>,
+  <SwiperSlide key={2}><img src={img} /></SwiperSlide>,
+  <SwiperSlide key={1}><img src={img} /></SwiperSlide>,
 ];
 
 const Carrousel = () => {
-      const [showText, setShowText] = useState(false);
+  const [showText, setShowText] = useState(false);
 
-      const handleMouseOver = () => {
-        setShowText(true);
-      };
+  const handleMouseOver = () => {
+    setShowText(true);
+  };
 
-      const handleMouseOut = () => {
-        setShowText(false);
-      };
-      const swiperOptions = {
-        loop:true,
-        navigation:true,
-        spaceBetween:-130,
-        slidesPerView:3,
-        centeredSlides:true,
-      }
-      
+  const handleMouseOut = () => {
+    setShowText(false);
+  };
+  const swiperOptions = {
+    loop: true,
+    navigation: true,
+    spaceBetween: -130,
+    slidesPerView: 3,
+    centeredSlides: true,
+  }
 
-      SwiperCore.use([Navigation]);
-      return (
-        
-        <div className='mainDiv'>
-          <div className='imgLogo'>
-          <img className='image_carrousel' src={image} alt="Logo"/> 
-          </div>
-          <div className='Carre'>
-          { <p className='mt-4'>Danses</p> }
-        </div>
-        <div className='background'>
-          {/* <img id='img' src={"./background.png"}></img> */}
+
+  SwiperCore.use([Navigation]);
+  return (
+
+    <div className='mainDiv'>
+      <div className='imgLogo'>
+        <img className='logo_carrousel' src={logo} alt="Logo" />
+      </div>
+      <div className='Carre'>
+        {<p className='mt-4'>Danses</p>}
+      </div>
+      <div className='background'>
+        {/* <img id='img' src={"./background.png"}></img> */}
         <Swiper {...swiperOptions}
+          className='swiper-container'
+          style={{
+            width: "1350px",
+            height: "500px",
+            marginTop: "30px",
+          }}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {
+            slides.map((Slide, index) => (
+              <SwiperSlide key={index} virtualIndex={index}><img className="caroussel_image" src={img} /></SwiperSlide>
+            ))
+          }
+        </Swiper>
+      </div>
+    </div>
 
-                className='swiper-container'
-                style={{width: "1350px",
-                height: "500px",
-                marginTop: "30px",
-                }}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-              >
-              {
-                slides.map((Slide,index)=>(
-                 <SwiperSlide key={index} virtualIndex={index}><img src={img}/></SwiperSlide>
-                ))
-              }
-              </Swiper>
-              </div>
-              </div>
-              
-      )
-    }
-  export default Carrousel
+  )
+}
+export default Carrousel
