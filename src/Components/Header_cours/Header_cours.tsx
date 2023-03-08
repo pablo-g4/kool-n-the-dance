7c852925c500934e7af0b479e37ddb0b284bc456
danyGoncalvesNovais <91186956+danyGoncalvesNovais@users.noreply.github.com>
2023-03-08 13:57:05 +0100

    cards + fond

--- src/Components/Header_cours/Header_cours.tsx
+++ src/Components/Header_cours/Header_cours.tsx
@@ -1,6 +1,7 @@
 import React, { Component, useState } from "react";
 import "./Header_cours.css";
 import image from "./test.png";
+import vaguerose from "./VagueRose.png";
 
 const Header_cours = () => {
 const [isHovering, setIsHovering] = useState(false);
@@ -9,34 +10,41 @@ const handleMouseLeave = () => {setIsHovering(false);};
 
 
 
+
   
 
     return (
         <>
-          <div className="titlecours">
-          <h1><b>Cours</b></h1>
-          </div>
-          <div className="groupe">
-            <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{transform: isHovering ? 'translate(-70px, 0px)' : 'none',}}>
-              <img src={image} className="card-img-top" />
-            </div>
-            <div className="textdanses" style={{transform: isHovering ? 'translate(-70px, 0px)' : 'none',}}>
-              <p>Danses</p>
-            </div>
-            <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{transform: isHovering ? 'translate(-10px, 0px)' : 'none',}}>
-              <img src={image} className="card-img-top" /> 
-            </div>
-            <div className="textfitness" style={{transform: isHovering ? 'translate(-10px, 0px)' : 'none',}}>
-              <p>Fitness</p>
-            </div>
-            <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  style={{transform: isHovering ? 'translate(60px, 0px)' : 'none',}}>
-              <img src={image} className="card-img-top" />
-            </div>
-            <div className="textforfait" style={{transform: isHovering ? 'translate(60px, 0px)' : 'none',}}>
-              <p>Forfait</p>
-            </div>
-          </div>
-        </>
+            <header>
+
+                <div className="titlecours">
+                <h1><b>Cours</b></h1>
+                </div>
+                <div className="groupe">
+                  <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{transform: isHovering ? 'translate(-70px, 0px)' : 'none',}}>
+                    <img src={image} className="card-img-top" />
+                  </div>
+                  <div className="textdanses" style={{transform: isHovering ? 'translate(-70px, 0px)' : 'none',}}>
+                    <p>Danses</p>
+                  </div>
+                  <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{transform: isHovering ? 'translate(-10px, 0px)' : 'none',}}>
+                    <img src={image} className="card-img-top" /> 
+                  </div>
+                  <div className="textfitness" style={{transform: isHovering ? 'translate(-10px, 0px)' : 'none',}}>
+                    <p>Fitness</p>
+                  </div>
+                  <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  style={{transform: isHovering ? 'translate(60px, 0px)' : 'none',}}>
+                    <img src={image} className="card-img-top" />
+                  </div>
+                  <div className="textforfait" style={{transform: isHovering ? 'translate(60px, 0px)' : 'none',}}>
+                    <p>Forfait</p>
+                  </div>
+                </div>
+                <div>
+                  <img  className = "vague" src={vaguerose} />
+                </div>
+            </header>
+          </>
     );
   
 }
