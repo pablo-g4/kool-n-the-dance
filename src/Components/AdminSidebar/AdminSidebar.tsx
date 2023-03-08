bad99c193d3c9dd37b8fbab5456fe1f19e2f5175
Allan COSTES <allancts.compte@gmail.com>
2023-03-08 17:00:13 +0100

    update AdminSidebar

--- src/Components/AdminSidebar/AdminSidebar.tsx
+++ src/Components/AdminSidebar/AdminSidebar.tsx
@@ -3,9 +3,17 @@ import "./AdminSidebar.css";
 
 const AdminSidebar = () => {
   return (
+
+    //Ajouter le className 'currentPage' à l'onglet correspondant à la page active
+    //
+    //Ajouter l'image à aSidebar_logo
+    //
+    //Ajouetr les bonnes polices
+    //
+    //Essayer de faire les flêches sur l'onglet actif
     <div className="aSidebar">
       <div className="aSidebar_logo">img</div>
-      <a className="aSidebar_onglet currentPage" href="#">
+      <a className="aSidebar_onglet" href="#">
         <p className="aSidebar_text">Gestion Général</p>
       </a>
       <a className="aSidebar_onglet" href="#">
