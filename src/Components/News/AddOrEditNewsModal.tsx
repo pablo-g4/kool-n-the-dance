a8906908cb6aea715a7f6ae78b3ba0fc5fbea252
Pablo MDS <PabloOffWork@Air-de-Pablo.lan>
2023-03-09 08:50:30 +0100

    pushing new component

---
+++ src/Components/News/AddOrEditNewsModal.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, Group, Button } from '@mantine/core';
+import { News } from '../../Models/News'
+import { createNews } from '../../Controllers/news';
+
+const AddOrEditNewsModal = () => {
+    const [opened, { open, close }] = useDisclosure(false);
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+    
+        const title = event.target.elements.title.value;
+        const description = event.target.elements.description.value;
+        const attachedFile = event.target.elements.attachedFile.value;
+        const isActive = event.target.elements.isActive.value;
+    
+        let newNews = new News()
+    
+        newNews.title = title
+        newNews.description = description
+        newNews.attachedFile = attachedFile
+        newNews.isActive = isActive
+    
+        const createdNews = await createNews(newNews)   
+        console.log('createdNews', createdNews);
+         
+        
+      };
+
+    return (
+        <>
+            <Modal opened={opened} onClose={close} title="Ajouter actualité" centered>
+                <form onSubmit={handleSubmit} action=''>
+                    <label htmlFor='title'>Titre</label>
+                    <input type="text" name='title' required></input><br></br>
+
+                    <label>Description</label>
+                    <input type="text" name='description' required></input><br></br>
+
+                    <label htmlFor='attachedFile'>Ajouter un fichier</label>
+                    <input type="file" id='attachedFile' name='attachedFile'></input><br></br>
+
+                    <label>active ?</label><br></br>
+
+                    <input type="radio" id="isActive" name="isActive" value="true" />
+                    <label>Oui</label><br></br>
+                    <input type="radio" id="avtive" name="isActive" value="false" />
+                    <label>Non</label><br></br>
+
+                    <input type="submit" value="submit"></input><br></br>
+                </form>
+            </Modal>
+
+            <Group position="center">
+                <Button onClick={open}>Ajouter actualité</Button>
+            </Group>
+        </>
+    )
+}
+
+export default AddOrEditNewsModal
