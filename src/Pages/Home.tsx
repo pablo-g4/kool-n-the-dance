4ab3140ccebe0f8bfb09f705fd73e270babf88a0
Nathan Hyppolite <ihyppolite971@gmail.com>
2023-03-09 09:20:08 +0100

    grid pour image

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import Footer from '../Components/Footer/Footer'
+import React from "react";
+import Footer from "../Components/Footer/Footer";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home page
+    <div>
+      <div className="parent">
+        <div className="div1">
+          <p>1</p>{" "}
+        </div>
+        <div className="div2">
+          <p>2</p>{" "}
+        </div>
+        <div className="div3">
+          {" "}
+          <p>3</p>
+        </div>
+        <div className="div4">
+          <p>4</p>{" "}
+        </div>
+        <div className="div5">
+          <p>5</p>{" "}
+        </div>
+        <div className="div6">
+          <p>6</p>{" "}
+        </div>
+        <div className="div7">
+          <p>7</p>{" "}
+        </div>
+        <div className="div8">
+          <p>8</p>{" "}
+        </div>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
