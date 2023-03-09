69850209555c52259439bea3c2d13d5a87222fee
Pablo MDS <PabloOffWork@Air-de-Pablo.lan>
2023-03-09 09:15:52 +0100

    fixing images preview

--- src/Pages/Galerie/Galerie.jsx
+++ src/Pages/Galerie/Galerie.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useCallback } from 'react';
 import Footer from '../../Components/Footer/Footer'
 import { Carousel } from '@mantine/carousel';
-import carousel1  from '../../Assets/Images/carousel/carousel1.png';
-import carousel2  from '../../Assets/Images/carousel/carousel2.png';
-import carousel3 from '../../Assets/Images/carousel/carousel3.png';
-import carousel4  from '../../Assets/Images/carousel/carousel4.png';
+import carousel1  from '../../Assets/Images/carousel/dance1.jpg';
+import carousel2  from '../../Assets/Images/carousel/dance2.jpg';
+import carousel3 from '../../Assets/Images/carousel/dance3.jpg';
+import carousel4  from '../../Assets/Images/carousel/dance4.jpg';
 import carousel5  from '../../Assets/Images/carousel/carousel5.png';
 import carousel6  from '../../Assets/Images/carousel/carousel6.png';
 import { render } from 'react-dom';
@@ -16,6 +16,8 @@ import ImageViewer from 'react-simple-image-viewer';
 const Galerie = () => {
   const [currentImage, setCurrentImage] = useState(0);
   const [isViewerOpen, setIsViewerOpen] = useState(false);
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
   const images = [
     carousel1,
     carousel2,
@@ -24,6 +26,9 @@ const Galerie = () => {
     carousel5,
     carousel6,
   ];
+
+  const filteredImages = [images[activeImageIndex]]
+  console.log('filteredImages', filteredImages);
 
   const openImageViewer = useCallback((index) => {
     setCurrentImage(index);
@@ -34,6 +39,11 @@ const Galerie = () => {
     setCurrentImage(0);
     setIsViewerOpen(false);
   };
+
+  const handleSlideChange = (index) => {
+    console.log(index);
+    setActiveImageIndex(index)
+  }
   return (
     
     <div className='body'>
@@ -41,13 +51,12 @@ const Galerie = () => {
 
         <div className='visualiseuse'>
 
-          {images.map((src, index) => (
+          {filteredImages.map((src, index) => (
           <img
             src={ src }
             onClick={ () => openImageViewer(index) }
-            width="100"
             key={ index }
-            style={{ margin: '2px' }}
+            style={{ margin: '2px', width: '800px' }}
             alt=""
           />
         ))}
@@ -55,17 +64,17 @@ const Galerie = () => {
         {isViewerOpen && (
           <ImageViewer
             src={ images }
-            currentIndex={ currentImage }
+            currentIndex={ activeImageIndex }
             disableScroll={ false }
             closeOnClickOutside={ true }
             onClose={ closeImageViewer }
           />
-        )}
+        )} 
 
         </div>
 
         <div className='carou'>
-          <Carousel orientation="vertical" loop="true" height={545} maw={320} dragFree slideSize="13.5%" initialSlide={2} >
+          <Carousel orientation="vertical" loop="true" height={545} onSlideChange={ (index) => handleSlideChange(index)} maw={320} dragFree slideSize="13.5%" initialSlide={2} >
             <Carousel.Slide>
               <img src={carousel1} height="110.2px" width="180px" ></img>
             </Carousel.Slide>
