4151b1057b8cb1bceecc581e23a1eedae67b586a
Allan COSTES <allancts.compte@gmail.com>
2023-03-09 11:27:20 +0100

    add AdminSidebar

---
+++ src/Components/AdminSidebar/AdminSidebar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./AdminSidebar.css";
+import LogoBlanc from "../../image/LogoBlanc.png";
+
+const AdminSidebar = () => {
+  return (
+
+    //Ajouter le className 'currentPage' à l'onglet correspondant à la page active
+    //
+    //Essayer de faire les flêches sur l'onglet actif
+    //
+    //D'après la maquettes enlevé Navbar (top) et footer
+    <div className="aSidebar">
+      <div className="aSidebar_logo"><img className='LogoBlanc' src={LogoBlanc}></img></div>
+      <a className="aSidebar_onglet" href="#">
+        <p className="aSidebar_text">Gestion Général</p>
+      </a>
+      <a className="aSidebar_onglet" href="#">
+        <p className="aSidebar_text">Gestion Cours</p>
+      </a>
+      <a className="aSidebar_onglet" href="#">
+        <p className="aSidebar_text">Gestion Planning</p>
+      </a>
+      <a className="aSidebar_onglet" href="#">
+        <p className="aSidebar_text">Gestion Galerie</p>
+      </a>
+      <a className="aSidebar_onglet" href="#">
+        <p className="aSidebar_text">Gestion Actualité</p>
+      </a>
+      <a className="aSidebar_onglet" href="#">
+        <p className="aSidebar_text">Gestion Forfait</p>
+      </a>
+    </div>
+  );
+};
+export default AdminSidebar;
--- src/Pages/Admin/Planning/Planning.tsx
+++ src/Pages/Admin/Planning/Planning.tsx
@@ -1,21 +1,22 @@
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import "./Planning.css"
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import AdminSidebar from "../../../Components/AdminSidebar/AdminSidebar";
 
 
 const Planning = () => {
   return (
-
-    
     <>
-    <div className='centrage'>
-    <div className='text-haut '  style={{ }}>Editez votre planning sur</div>
-    
-    <button type="button" className="button-bas btn">Google Agenda <FontAwesomeIcon icon={faArrowRight} /> </button>
-
-    </div>
+      <div className="d-flex flex-column- ">
+        <div className="col-3 p-0">
+          <AdminSidebar></AdminSidebar>
+        </div>
+        <div className='centrage'>
+          <div className='text-haut '  style={{ }}>Editez votre planning sur</div>
+          <button type="button" className="button-bas btn">Google Agenda <FontAwesomeIcon icon={faArrowRight} /> </button>
+        </div>
+      </div>
     </>
     
   )
