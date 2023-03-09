8ed391c971f512bae800745802f0b155649abdbd
Allan COSTES <allancts.compte@gmail.com>
2023-03-09 17:07:35 +0100

    Update Planning.tsx

--- src/Pages/Admin/Planning/Planning.tsx
+++ src/Pages/Admin/Planning/Planning.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import "./Planning.css"
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import AdminSidebar from "../../../Components/AdminSidebar/AdminSidebar";
 
 
 const Planning = () => {
@@ -10,12 +11,17 @@ const Planning = () => {
 
     
     <>
-    <div className='centrage'>
-    <div className='text-haut '  style={{ }}>Editez votre planning sur</div>
-    
-    <button type="button" className="button-bas btn">Google Agenda <FontAwesomeIcon icon={faArrowRight} /> </button>
+      <div className="d-flex flex-column- ">
+        <div className="col-3 p-0">
+          <AdminSidebar></AdminSidebar>
+        </div>
+        <div className='centrage'>
+        <div className='text-haut '  style={{ }}>Editez votre planning sur</div>
+        
+        <button type="button" className="button-bas btn">Google Agenda <FontAwesomeIcon icon={faArrowRight} /> </button>
 
-    </div>
+        </div>
+      </div>
     </>
     
   )
