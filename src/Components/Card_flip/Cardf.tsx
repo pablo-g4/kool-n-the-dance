2d7b7c4fb72023c9975619e94bb456fc17ffa15a
valentinfo <gago391@gmail.com>
2023-03-10 15:07:57 +0100

    push card

--- src/Components/Card_flip/Cardf.tsx
+++ src/Components/Card_flip/Cardf.tsx
@@ -35,15 +35,12 @@ function CardFlip() {
           </div>
         </div>
         <div>
-          <div className="cardCours">
-            <p className="titreCours">Zumba</p>
-            <div className="imgCours">
+          <div className="cardCours2">
+            
+            <div className="imgCours2">
               <p>
-                Programme d'entraînement physique complet, alliant tous les
-                éléments de la remise en forme : cardio et préparation musculaire,
-                équilibre et flexibilité. Les chorégraphies s'inspirent
-                principalement des danses latines mais aussi de styles variés
-                comme la danse orientale, disco, hip hop...
+             
+               
               </p>
               <div className="mt-4 row justify-content-end">
                 <img className="flecheRe" src={fleche} alt="fleche" onClick={handleClick} style={{ cursor: 'pointer' }} />
