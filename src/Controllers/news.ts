cfd1c5629d631806bcf7c41bd23526fe9c6ca3a8
Pablo MDS <PabloOffWork@Air-de-Pablo.lan>
2023-03-13 10:14:50 +0100

    news controllers

--- src/Controllers/news.ts
+++ src/Controllers/news.ts
@@ -1,16 +1,27 @@
-import { COLLECTION } from '../db/collection';
-import { getAllDataFromCollection, addDocumentToCollection } from '../db/dbHelper'
-import { News } from '../Models/News';
+import { COLLECTION } from '../db/collection'
+import { getAllDataFromCollection, addDocumentToCollection, updateDocumentToCollection } from '../db/dbHelper'
+import { News } from '../Models/News'
 
-export const createNews = async (news: News) => {    
+export const createNews = async (news: News): Promise<string> => {    
+
     const createdNewsId = await addDocumentToCollection(COLLECTION.NEWS, news.toDb())
     return createdNewsId
+
 }
 
-export const getAllNews = async ():Promise<News[]> => {
+export const updateNews = async (news: News): Promise<void> => {  
+
+    await updateDocumentToCollection(COLLECTION.NEWS, news.id, news.toDb())
+
+}
+
+export const deleteNews = async (newsId: string): Promise<void> => {
+    
+}
+
+export const getAllNews = async (): Promise<News[]> => {
 
     const allNews = await getAllDataFromCollection(COLLECTION.NEWS)
-    console.log('allNews', allNews);
     
     return allNews.map((news: any) => {
         return News.fromDb(news)
