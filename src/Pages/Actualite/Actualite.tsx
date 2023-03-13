e022538d56528e6eb2415c9ff7bfca9a0a46730d
Pablo MDS <PabloOffWork@Air-de-Pablo.lan>
2023-03-13 10:15:24 +0100

    actualite page

--- src/Pages/Actualite/Actualite.tsx
+++ src/Pages/Actualite/Actualite.tsx
@@ -7,16 +7,15 @@ import { useCallback, useEffect } from 'react';
 import AddOrEditNewsModal from '../../Components/News/AddOrEditNewsModal';
 import { News } from '../../Models/News';
 import {  Group, Button } from '@mantine/core';
+import DeleteConfirmationModal from '../../Components/Global/DeleteConfirmationModal';
+import { deleteNews } from '../../Controllers/news';
 
 const Actualite = () => {
 
-  const[allNews, setAllNews] = useState<News[]>([])
-  const[currentNews, setCurrentNews] = useState<News>()
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleCurrentNews = (currentNews:News) => {
-    setCurrentNews(currentNews);
-  }
+  const [allNews, setAllNews] = useState<News[]>([])
+  const [currentNews, setCurrentNews] = useState<News | undefined>(undefined)
+  const [isAddOrEditModalOpen, setIsAddOrEditModalOpen] = useState(false);
+  const [isDeleteConfirmationModalOpen, setIsDeleteConfirmationModalOpen] = useState(false);
 
   const fetchData = useCallback(async () => {
     const news = await getAllNews()
@@ -25,8 +24,29 @@ const Actualite = () => {
     }
   }, [])
 
+  const closeAddOrEditModal = () => {
+    setIsAddOrEditModalOpen(false)
+    setCurrentNews(undefined)
+  }
+
+  const closeDeleteConfirmationModal = () => {
+    setIsDeleteConfirmationModalOpen(false)
+  }
+
+  const openDeleteConfirmationModal = () => {
+    setIsDeleteConfirmationModalOpen(true)
+  }
+
+  const deleteCurrentNews = () => {
+    setAllNews((oldNewsState) => oldNewsState.filter(newsItem => newsItem.id !== currentNews?.id ))
+    setCurrentNews(undefined)
+    closeDeleteConfirmationModal()
+  }
+
   useEffect(() => {
     fetchData()
+    // console.log('i fire once....');
+    
   }, []);
 
   return (
@@ -35,20 +55,23 @@ const Actualite = () => {
       <div className='row-v2'>
         <div className='col-md-7 col-xs-11'>
           {
-            allNews.map((news, index) => (
-              <Card news={news} setCurrentNews={setCurrentNews} key={index} />
-            ))
+            allNews.map((news, index) => <Card news={news} setCurrentNews={setCurrentNews} setIsOpen={setIsAddOrEditModalOpen} displayDeleteConfirmationModal={openDeleteConfirmationModal} key={index} />)
           }
         </div>
         <div className='col-5 d-none d-md-block'>
-          <CardRight/>
+          <CardRight />
         </div>
       </div>
-        <Group position="center">
-          <Button onClick={() => setIsOpen(true)}>Ajouter actualité</Button>
-         </Group>
-      {isOpen &&
-        <AddOrEditNewsModal currentNews={currentNews} setAllNews={setAllNews} isOpen={isOpen} setIsOpen={setIsOpen} />
+      <Group position="center">
+        <Button onClick={() => setIsAddOrEditModalOpen(true)}>Ajouter actualité</Button>
+      </Group>
+      {
+        isAddOrEditModalOpen &&
+        <AddOrEditNewsModal currentNews={currentNews} setAllNews={setAllNews} isOpen={isAddOrEditModalOpen} setIsOpen={closeAddOrEditModal} />
+      }
+      {
+        isDeleteConfirmationModalOpen && 
+        <DeleteConfirmationModal isOpen={isDeleteConfirmationModalOpen} closeModal={closeDeleteConfirmationModal} deleteCurrentNews={deleteCurrentNews} />
       }
     </div>
   )
