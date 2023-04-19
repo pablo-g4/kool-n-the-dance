6b1b9174ed361d641762c6120826c0ffa58f5ed1
Ben <benbugarindev@gmail.com>
2023-04-19 11:41:25 +0200

    navbar fix

--- src/Pages/Layouts/Layout.tsx
+++ src/Pages/Layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Navbar from '../../Components/Navbar/Navbar'
+import Navbar2 from '../../Components/Navbar/Navbar2'
 import Footer from '../../Components/Footer/Footer'
 
 
