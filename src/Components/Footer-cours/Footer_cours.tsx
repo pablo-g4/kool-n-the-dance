8065f933875b8dc88afaab28256ef68805a59e70
Pablo MDS <PabloOffWork@MacBook-Air-de-Pablo.local>
2023-04-25 09:24:23 +0200

    using dynamic values

--- src/Components/Footer-cours/Footer_cours.tsx
+++ src/Components/Footer-cours/Footer_cours.tsx
@@ -1,49 +1,25 @@
 import React, { Component } from "react";
+import { Forfait } from "../../Models/Forfait";
 import "./Footer_cours.css";
 
-const Footer_cours = () => {
+const Footer_cours = ({ forfaits } : { forfaits?: Forfait[]}) => {
   return (
     <>
       <div className="red-bg pt-5">
-        <div className=" row ">
-          <div className="col-md-4 col-xs-12">
-            <p className="p-cours-titre">Forfait Danse Enfant</p>
-            <div>
-              <p>2 cours : 370 €/an</p>
-              <p>3 cours : 450 €/an</p>
-            </div>
-
-
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <p className="p-cours-titre">Forfait Danses Adultes</p>
-            <div>
-              <p>2 cours : 460 €/an </p>
-              <p>3 cours : 650 €/an </p>
-              <p>4 cours : 850 €/an </p>
-            </div>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <p className="p-cours-titre">Forfait Danses et Fitness</p>
-            <div>
-              <p>1 cours Danse + 1 cours Fitness : 420 €/an</p>
-            </div>
-          </div>
-          <div className="col-md-4 col-xs-12">
-            <p className="p-cours-titre"> Full Pass</p>
-            <div>
-              <p>Accès à tous les cours : 990 €/an</p>
-            </div>
-          </div>
-          
-          <div className="col-md-4 col-xs-12">
-              <p className="p-cours-titre">Forfait Duo</p>
+        <div className="row ">
+          {
+            forfaits?.length && forfaits?.map((forfait) => (
+              <div className="col-md-4 col-xs-12">
+              <p className="p-cours-titre">{forfait.title}</p>
               <div>
-                <p>
-                  Pass Parent+Enfant sur cours Zumba ou PortDeBras : 395 €/an
-                </p>
+                <p style={{
+                  whiteSpace: 'pre-line'
+                }
+                }>{forfait.description.split(/\r?\n|\r|\n/g)}</p>
               </div>
             </div>
+            ))
+          }
         </div>
         <div className="d-grid text-center gap-2 col-6 mx-auto">
           <button className="footer-button-cours" type="button">
--- src/Pages/Cours/Cours.tsx
+++ src/Pages/Cours/Cours.tsx
@@ -1,13 +1,26 @@
-import React from "react";
-import Footer from "../../Components/Footer/Footer";
-import Header_cours from "../../Components/Header_cours/Header_cours";
-import Footer_cours from "../../Components/Footer-cours/Footer_cours";
-
-import Carrousel_Activite from "../../Components/Carrousel_Activite/Carrousel";
-import Carrousel_Yellow from "../../Components/Carrousel_Activite/Carrousel_Yellow";
-import Carrousel_Red from "../../Components/Carrousel_Activite/Carrousel_Red";
+import _ from "lodash"
+import React, { useEffect, useState } from "react"
+import Header_cours from "../../Components/Header_cours/Header_cours"
+import Footer_cours from "../../Components/Footer-cours/Footer_cours"
+import Carrousel_Activite from "../../Components/Carrousel_Activite/Carrousel"
+import Carrousel_Yellow from "../../Components/Carrousel_Activite/Carrousel_Yellow"
+import Carrousel_Red from "../../Components/Carrousel_Activite/Carrousel_Red"
+import { getAllForfaits } from "../../Controllers/forfait"
+import { Forfait } from "../../Models/Forfait"
 
 const Cours = () => {
+
+  const [forfaits, setForfaits] = useState<Forfait[]>()
+
+  const fetchData = async () => {
+    const allForfaits = await getAllForfaits()
+    setForfaits(allForfaits)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div>
       <Header_cours/>
@@ -15,7 +28,11 @@ const Cours = () => {
 
       <Carrousel_Yellow/>
       <Carrousel_Red/> 
-      <Footer_cours />
+      {
+        _.filter(forfaits, 'isBasic')?.length ? (
+          <Footer_cours forfaits={_.filter(forfaits, 'isBasic')}/>
+        ) : ('Pas de forfaits disponibles')
+      }
     </div>
   );
 };
